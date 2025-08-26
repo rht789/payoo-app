18ce0ad3b87c054e797510b861b123cbe57b4df2
rezaulkarim <rezaulrahaat@gmail.com>
2025-08-26 20:47:37 +0600

    Implement login functionality and add a simple home page skeleton

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.getElementById("loginButton").addEventListener('click', function(e) {
     e.preventDefault();
-    console.log("Login button clicked");
+    const mobileNumber = 12345678901;
+    const mobileNumberConverted = parseInt(document.getElementById("mobile-number").value);
+
+    const pinNumber = 1234;
+    const pinNumberConverted = parseInt(document.getElementById("pin-number").value);
+
+    if(mobileNumberConverted === mobileNumber && pinNumberConverted === pinNumber) {
+        window.location.href = "./home.html";
+    }
+    else {
+        alert("Invalid mobile number or pin number");
+    }
 });
