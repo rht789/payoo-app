d02bb31287e6cf4856cf0c9571ba6032e55f9955
rezaulkarim <rezaulrahaat@gmail.com>
2025-08-27 10:07:00 +0600

    Add JavaScript functionality for 'Add Money' button and update available balance

---
+++ home.js
@@ -0,0 +1,19 @@
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const bank = document.getElementById("bank").value;
+    const accountNumber = document.getElementById("account-number").value;
+
+    const amount = parseInt(document.getElementById("add-amount").value);
+
+    const pinNumber = parseInt(document.getElementById("add-pin").value);
+
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+    
+    const totalNewAvailableBalance = amount + availableBalance;
+    document.getElementById("available-balance").innerText =
+      totalNewAvailableBalance;
+  });
